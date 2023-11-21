8ea6de990c6f34b6299f8820ceb7cba2b1eca1a8
Jordan Acosta <jordanjacosta@gmail.com>
2023-11-21 11:34:59 -0800

    fix: update nuon TF

--- nuon/docker_build_component.tf
+++ nuon/docker_build_component.tf
@@ -1,9 +1,9 @@
-resource "nuon_docker_build_component" "example" {
-  name   = "Example"
+resource "nuon_docker_build_component" "dockerfile" {
+  name   = "dockerfile"
   app_id = nuon_app.example.id
 
   dockerfile = "Dockerfile"
-  connected_repo = {
+  public_repo = {
     directory = "example/docker"
     repo      = var.example_app_repo
     branch    = "main"
