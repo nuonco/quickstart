a2978ff0280311d88ccc1ec919d0b5845023254a
Jon Morehouse <MorehouseJ09@gmail.com>
2023-11-29 18:08:17 -0600

    fix: apply changes from fork (#14)
    * chore: fix quickstart (#8)
    
    * chore: test quickstart (#9)

--- nuon/app.tf
+++ nuon/app.tf
@@ -1,19 +1,3 @@
 resource "nuon_app" "example" {
   name = "quickstart_test_app"
 }
-
-resource "nuon_app_sandbox" "main" {
-  app_id            = nuon_app.example.id
-  terraform_version = "v1.6.3"
-
-  public_repo = {
-    repo      = "nuonco/sandboxes"
-    branch    = "main"
-    directory = "aws-eks"
-  }
-
-  input {
-    name  = "eks_version"
-    value = "v1.27.8"
-  }
-}
